105fe91b24b77111d53604323c7424fc7dd3b406
Jake Lovelace <jaglovelace@gmail.com>
2018-05-17 15:56:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,4 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(days, holiday) {
-  return `It's ${days} days until ${holiday}!`
-}
+
